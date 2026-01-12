d818cf96e0722d4daeebabad36acd47ca7ec4e5e
Sammy <sammy@kozykoding.com>
2026-01-11 17:29:01 -0800

    created child class leafnode and added tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,19 +1,13 @@
-
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
+
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(
-            "div", 
-            "Hello, world!", 
-            None, 
-            {"class": "greeting", "id": "main"}
+            "div", "Hello, world!", None, {"class": "greeting", "id": "main"}
         )
-        self.assertEqual(
-            node.props_to_html(), 
-            ' class="greeting" id="main"'
-        )
+        self.assertEqual(node.props_to_html(), ' class="greeting" id="main"')
 
     def test_values(self):
         node = HTMLNode("p", "This is a paragraph")
@@ -26,8 +20,23 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("a", "link", None, {"href": "https://google.com"})
         self.assertEqual(
             node.__repr__(),
-            "HTMLNode(a, link, children: None, {'href': 'https://google.com'})"
+            "HTMLNode(a, link, children: None, {'href': 'https://google.com'})",
         )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
