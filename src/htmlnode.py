3d7034a9f584d30e87a9098bcd6f0e061ef5e57c
Sammy <sammy@kozykoding.com>
2026-01-12 08:54:13 -0800

    cleaned up htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -57,21 +57,3 @@ class ParentNode(HTMLNode):
 
     def __repr__(self):
         return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
-
-
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(None, text_node.text)
-    if text_node.text_type == TextType.BOLD:
-        return LeafNode("b", text_node.text)
-    if text_node.text_type == TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-    if text_node.text_type == TextType.CODE:
-        return LeafNode("code", text_node.text)
-    if text_node.text_type == TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": text_node.link})
-    if text_node.text_type == TextType.IMAGE:
-        return LeafNode(
-            "img", text_node.text, {"src": text_node.link, "alt": text_node.alt}
-        )
-    raise Exception(f"Invalid text type: {text_node.text_type}")
